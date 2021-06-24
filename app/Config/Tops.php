f68478d856648342291eb9ae1bb6241a32056af8
labello91 <80860058+labello91@users.noreply.github.com>
2021-06-24 16:10:13 -0500

    Update Tops.php

--- app/Config/Tops.php
+++ app/Config/Tops.php
@@ -10,7 +10,7 @@ class Tops extends BaseConfig {
 
     public $app_settings_array = array(
         "app_version" => "2.7.1",
-        "app_update_url" => 'https://releases.tops.com/tops/',
+        "app_update_url" => 'https://releases.fairsketch.com/rise/',
         "updates_path" => './updates/',
     );
     public $app_csrf_exclude_uris = array(
